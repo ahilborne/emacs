91aa76aacb2e5f61ce7dac804950be6e4be4f3ea
Andrew <amh@ubuntu-18.04>
2018-09-21 17:01:34 +0100

    init.el custom.el: WIP; we think package-initialize does too much M
    - it "activates" packages. Aka add to load-path and load their autload
      files. And the list of packages to do this
      for (package-selected-packages) is, we believe, populated by
      use-package when it _does_ install a package.
    - it could be useful (to other people) if use-package didn't add to
      the list of installed packages, and maybe if :defer t, or another
      keyword, could delay download of packages until an attempt to
      autoload them.
    - However, we don't care enough and believe our {init.el, custom.el},
      with or without /elpa/, will start up and do the right thing on a
      new machine with the right network connectivity.

--- init.el
+++ init.el
@@ -1,7 +1,6 @@
 ;; START - especially load-path
 ;; --------------------------------------
 
-
 ; These stanzas enable us to run from an init file which isn't ~/.emacs.d/init.el. Like this:
 ;       emacs -q -l "this file"
 (setq user-init-file (or load-file-name (buffer-file-name))
@@ -19,13 +18,16 @@
 (add-to-list 'package-archives
        '("melpa" . "http://melpa.org/packages/") t)
 
+(setq package-enable-at-startup nil)
 (package-initialize)
 (when (not package-archive-contents)
   (package-refresh-contents))
 
 (defvar myPackages '(
     ; better-defaults
+    use-package
     material-theme))
+(setq use-package-always-ensure t)
 
 (mapc #'(lambda (package)
     (unless (package-installed-p package)
@@ -36,8 +38,17 @@
 ;; --------------------------------------
 
 (setq inhibit-startup-message t)        ;; hide the startup message
-(load-theme 'material t)                ;; load material theme
-; (global-linum-mode t)                 ;; enable line numbers globally
+(use-package material-theme
+	     :config
+(load-theme 'material t))                ;; load material theme
 
 ;; (set-face-attribute 'default nil :family "Courier" :foundry "Adobe" :height 87)
 (set-face-attribute 'default nil :family "Source Code Pro" :foundry "ADBO" :height 87)
+
+(use-package elpy
+  :config
+  (delete 'elpy-module-highlight-indentation elpy-modules)
+  (elpy-enable))
+(use-package magit
+  :bind ("C-x g" . magit-status))
+(use-package pipenv)
