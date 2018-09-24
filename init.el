3d4161f8dc1dfa6269ef78bdecf93ff5d9505dfb
Andrew <amh@ubuntu-18.04>
2018-09-24 18:39:01 +0100

    init.el: use https for melpa

--- init.el
+++ init.el
@@ -28,7 +28,7 @@
 (require 'package)
 
 (add-to-list 'package-archives
-       '("melpa" . "http://melpa.org/packages/") t)
+       '("melpa" . "https://melpa.org/packages/") t)
 
 (setq package-enable-at-startup nil)
 (package-initialize)
