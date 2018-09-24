70581f101ce566ed60946a46c492dd37fb596456
Andrew <amh@ubuntu-18.04>
2018-09-24 16:12:47 +0100

    custom.el: desktop-save-mode only works here

--- custom.el
+++ custom.el
@@ -3,9 +3,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(desktop-save-mode t)
  '(package-selected-packages
    (quote
-    (py-autopep8 pipenv magit elpy material-theme use-package))))
+    (filladapt dired-x hs-minor-mode py-autopep8 pipenv magit elpy material-theme use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
