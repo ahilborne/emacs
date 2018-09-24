05818fd9f0c6a9ca65ba88244afc4a3e97997a09
Andrew <amh@ubuntu-18.04>
2018-09-24 18:02:09 +0100

    custom.el: themes

--- custom.el
+++ custom.el
@@ -3,6 +3,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("a24c5b3c12d147da6cef80938dca1223b7c7f70f2f382b26308eba014dc4833a" "732b807b0543855541743429c9979ebfb363e27ec91e82f463c91e68c772f6e3" default)))
  '(desktop-save-mode t)
  '(package-selected-packages
    (quote
