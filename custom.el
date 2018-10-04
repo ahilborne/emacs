0893bae60f1420d43c121c4b864fe7884f5fe13b
Andrew Hilborne <andrew@hilborne.com>
2018-10-04 17:48:47 +0100

    init.el/custom.el: random

--- custom.el
+++ custom.el
@@ -7,9 +7,14 @@
    (quote
     ("a24c5b3c12d147da6cef80938dca1223b7c7f70f2f382b26308eba014dc4833a" "732b807b0543855541743429c9979ebfb363e27ec91e82f463c91e68c772f6e3" default)))
  '(desktop-save-mode t)
+ '(magit-log-arguments (quote ("--graph" "--decorate" "-n256")))
  '(package-selected-packages
    (quote
-    (filladapt dired-x hs-minor-mode py-autopep8 pipenv magit elpy material-theme use-package))))
+    (filladapt dired-x hs-minor-mode py-autopep8 pipenv magit elpy material-theme use-package)))
+ '(safe-local-variable-values
+   (quote
+    ((pyvenv-workon . tpy-dH-qQ_bn)
+     (pyvenv-workon . minos-EnCNK5mv)))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
