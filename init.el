b20e408cbb9ec34e56ef38b499fc4cba594dd10c
Andrew Hilborne <andrew.hilborne@powervault.co.uk>
2020-07-06 17:33:29 +0100

    Changes f6 → make a quick note

--- init.el
+++ init.el
@@ -576,13 +576,13 @@ Note well that this function _removes_ itself from the hs-minor-mode hook when i
 
 ;  (text-mode))
 (global-set-key (kbd "M-s s") 'jump-to-scratch)
-(global-set-key [f6] 'jump-to-scratch)
 
 ;; Create a quick note
 (defun make-quick-note()
   (interactive)
   (org-capture nil "Q"))
 (global-set-key (kbd "C-c q") 'make-quick-note)
+(global-set-key [f6] 'make-quick-note)
 
 ;; Jump to WIP.org
 (defun jump-to-WIP()
