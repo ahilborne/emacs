af05c4c708e77e744ceb90aed83785233efe57e3
Andrew Hilborne <andrew.hilborne@powervault.co.uk>
2020-10-01 16:09:32 +0100

    Don't try to group by .git repo in ibuffer-mode

--- init.el
+++ init.el
@@ -423,6 +423,7 @@
 	     (ibuffer-auto-mode 1)
              (ibuffer-switch-to-saved-filter-groups "std")))
 ;;           (ibuffer-vc-set-filter-groups-by-vc-root)))   ; Group by .git project
+                                                           ; Much too slow over TRAMP
 
 ;; Even more like e-buf-list - this puts cursor on last-changed buffer.
 ;; One of several solutions from https://www.emacswiki.org/emacs/IbufferMode.
