2a3574dfd34025a7f6fbf77c5a17bb8da0187ca7
Andrew Hilborne <andrew.hilborne@powervault.co.uk>
2020-10-01 16:11:04 +0100

    Sets conf-space-mode for Yocto .bb* files

--- init.el
+++ init.el
@@ -580,6 +580,7 @@ Note well that this function _removes_ itself from the hs-minor-mode hook when i
 (add-to-list 'auto-mode-alist '("\\.org$"  . org-mode))
 (add-to-list 'auto-mode-alist '("\\.ovpn$" . conf-space-mode))
 (add-to-list 'auto-mode-alist '("\\.conf$" . conf-space-mode))
+(add-to-list 'auto-mode-alist '("\\.bb\\(append\\|class\\)?$" . conf-space-mode))
 
 ;; One-handed (telephone) notebook
 (defun jump-to-scratch()
