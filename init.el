d2152518a8729a2581bdc5172c5aa2d33651193a
Andrew Hilborne <andrew.hilborne@powervault.co.uk>
2021-07-08 14:03:40 +0100

    catchup a lot of changes

--- init.el
+++ init.el
@@ -198,6 +198,8 @@
 ;;            :fetcher github
 ;;            :repo "markhepburn/mplayer-mode"))
 
+(use-package try)
+
 (use-package yaml-mode
   :config
   (add-to-list 'auto-mode-alist '("\\.yml$"   . yaml-mode))
@@ -212,79 +214,6 @@
 (use-package markdown-mode
   :config
   (setq markdown-command "pandoc --from=markdown --to=html --standalone --mathjax --highlight-style=pygments"))
-
-(use-package treemacs
-  :ensure t
-  :defer t
-  :init
-  (with-eval-after-load 'winum
-    (define-key winum-keymap (kbd "M-0") #'treemacs))
-  :config
-  (progn
-    (setq treemacs-collapse-dirs                 (if (executable-find "python3") 3 0)
-          treemacs-deferred-git-apply-delay      0.5
-          treemacs-display-in-side-window        t
-          treemacs-eldoc-display                 t
-          treemacs-file-event-delay              5000
-          treemacs-file-follow-delay             0.2
-          treemacs-follow-after-init             t
-          treemacs-git-command-pipe              ""
-          treemacs-goto-tag-strategy             'refetch-index
-          treemacs-indentation                   2
-          treemacs-indentation-string            " "
-          treemacs-is-never-other-window         nil
-          treemacs-max-git-entries               5000
-          treemacs-missing-project-action        'ask
-          treemacs-no-png-images                 nil
-          treemacs-no-delete-other-windows       t
-          treemacs-project-follow-cleanup        nil
-          treemacs-persist-file                  (expand-file-name ".cache/treemacs-persist" user-emacs-directory)
-          treemacs-recenter-distance             0.1
-          treemacs-recenter-after-file-follow    nil
-          treemacs-recenter-after-tag-follow     nil
-          treemacs-recenter-after-project-jump   'always
-          treemacs-recenter-after-project-expand 'on-distance
-          treemacs-show-cursor                   nil
-          treemacs-show-hidden-files             t
-          treemacs-silent-filewatch              nil
-          treemacs-silent-refresh                nil
-          treemacs-sorting                       'alphabetic-desc
-          treemacs-space-between-root-nodes      t
-          treemacs-tag-follow-cleanup            t
-          treemacs-tag-follow-delay              1.5
-          treemacs-width                         35)
-
-    ;; The default width and height of the icons is 22 pixels. If you are
-    ;; using a Hi-DPI display, uncomment this to double the icon size.
-    ;;(treemacs-resize-icons 44)
-
-    (treemacs-follow-mode t)
-    (treemacs-filewatch-mode t)
-    (treemacs-fringe-indicator-mode t)
-    (pcase (cons (not (null (executable-find "git")))
-                 (not (null (executable-find "python3"))))
-      (`(t . t)
-       (treemacs-git-mode 'deferred))
-      (`(t . _)
-       (treemacs-git-mode 'simple))))
-  ;; -amh- We don't need 'p' for 'previous line', so steal it for an easier-to-type 'peek'
-  :bind ([remap treemacs-previous-line] . treemacs-peek)
-  (:map global-map
-        ("M-0"       . treemacs)
-        ("C-x t 1"   . treemacs-delete-other-windows)
-        ("C-x t t"   . treemacs-select-window)
-        ("C-x t B"   . treemacs-bookmark)
-        ("C-x t C-t" . treemacs-find-file)
-        ("C-x t M-t" . treemacs-find-tag)))
-
-(use-package treemacs-projectile
-  :after treemacs projectile
-  :ensure t)
-
-;; (use-package treemacs-icons-dired
-;;   :after treemacs dired
-;;   :ensure t
-;;   :config (treemacs-icons-dired-mode))
 
 (use-package treemacs-magit
   :after treemacs magit
@@ -331,26 +260,29 @@
             (alltodo "" nil))
            nil)
 
-          ("w" "Weekly meetings"
-           ((tags-todo "thisW" nil)
-            (agenda "" nil))
-           ((org-agenda-tag-filter-preset (quote ("+thisW")))
-            (org-agenda-use-tag-inheritance nil)))
+          ;; ("w" "Weekly meetings"
+          ;;  ((tags-todo "thisW" nil)
+          ;;   (agenda "" nil))
+          ;;  ((org-agenda-tag-filter-preset (quote ("+thisW")))
+          ;;   (org-agenda-use-tag-inheritance nil)))
 
-          ("f" "Rev17 in factory quick hack"
-           ((tags-todo "factory" nil)
-            (agenda "" nil))
-           ((org-agenda-tag-filter-preset (quote ("+factory")))
-            (org-agenda-use-tag-inheritance nil)))
+          ;; ("f" "Rev17 in factory quick hack"
+          ;;  ((tags-todo "factory" nil)
+          ;;   (agenda "" nil))
+          ;;  ((org-agenda-tag-filter-preset (quote ("+factory")))
+          ;;   (org-agenda-use-tag-inheritance nil)))
 
           ("j" "New job!"
-           ((tags-todo "applications" nil)
+           ((tags-todo "todo" nil)
+            (tags-todo "applications" nil)
+            (tags-todo "home" nil)
             (tags-todo "newjob" nil)
             (agenda "" ((org-agenda-span 'day)) ))
            nil)
 
           ("J" "New job FULL!"
            ((tags-todo "applications" nil)
+            (tags-todo "home" nil)
             (tags-todo "newjob" nil)
             (tags-todo "cleanup" nil)
             (agenda "" ((org-agenda-span 'day)) ))
@@ -461,7 +393,7 @@
 ;; Default face
 ;; (set-face-attribute 'default nil :family "Courier" :foundry "Adobe" :height 87)
 ;; TODO install Source Code Pro first
-(set-face-attribute 'default nil :family "Source Code Pro" :foundry "ADBO" :height 87)
+;;; xxx (set-face-attribute 'default nil :family "Source Code Pro" :foundry "ADBO" :height 87)
 (setq kill-buffer-query-functions nil)
 
 ;; Global values for variables. Shouldn't be used for vars which can be
@@ -635,6 +567,14 @@
                           (progn
                             (setq sh-indent-for-case-label 0
                                   sh-indent-for-case-alt '+))))
+
+;; man-mode
+(require 'man)
+(progn
+  (setq
+   Man-switches "--nj")
+  (set-face-attribute 'Man-overstrike nil :inherit font-lock-type-face :bold t)
+  (set-face-attribute 'Man-underline nil :inherit font-lock-keyword-face :underline t))
 
 ;; hs-minor-mode
 (add-hook 'c-common-mode-hook (lambda () (hs-minor-mode 1)))
